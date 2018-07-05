9142f814a1a149b290fe1deb918dcb55b6df0bb4
Chengjiahang <261566462@qq.com>
2018-07-05 16:22:47 +0800

    Update

--- app/src/main/java/com/yuxindemo/testproject/util/DateUtil.java
+++ app/src/main/java/com/yuxindemo/testproject/util/DateUtil.java
@@ -1,5 +1,8 @@
 package com.yuxindemo.testproject.util;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by Administrator on 2018/7/5.
  */
@@ -8,8 +11,8 @@ public class DateUtil {
 
     // test
 
-    public void GetDate(){
-
+    public void GetDate(Context context){
+        Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show();
     }
 
 }
